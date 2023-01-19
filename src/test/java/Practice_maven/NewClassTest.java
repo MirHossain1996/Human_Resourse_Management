14fc4961966874d9267648079befb58c52790dc7
Sudhindra <Sudhindra@LAPTOP-VOGEQ752>
2023-01-19 13:05:59 +0530

    this is for a new class

---
+++ src/test/java/Practice_maven/NewClassTest.java
@@ -0,0 +1,12 @@
+package Practice_maven;
+
+import org.testng.annotations.Test;
+
+public class NewClassTest {
+
+	@Test
+	public void  classtest()
+	{
+		System.out.println("this is for classtest");
+	}
+}
