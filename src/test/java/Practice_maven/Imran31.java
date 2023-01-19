2b8b6dfb9eda969aa84ff92ddee0a9189b9af1bb
91851 <91851@DESKTOP-GDISGSQ>
2023-01-19 17:19:06 +0530

    tgrhty

---
+++ src/test/java/Practice_maven/Imran31.java
@@ -0,0 +1,12 @@
+package Practice_maven;
+
+import org.testng.annotations.Test;
+
+public class Imran31 {
+	@Test
+	public void firstMessage()
+	{
+		System.out.println("---HelloWorld---");
+		
+	}
+}
