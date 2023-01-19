fd9ced2c0360ff8c84548f69bcca3ea356273b34
91851 <91851@DESKTOP-GDISGSQ>
2023-01-19 17:00:49 +0530

    done

---
+++ src/test/java/Practice_maven/Imran3.java
@@ -0,0 +1,12 @@
+package Practice_maven;
+
+import org.testng.annotations.Test;
+
+public class Imran3 {
+	@Test
+	public void firstMessage()
+	{
+		System.out.println("---HelloWorld---");
+		
+	}
+}
